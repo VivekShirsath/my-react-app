cba7fe9223591977910cf7f6919e81e549d13f93
Vivek Shirsath <vivekshirsath1310@gmail.com>
2023-06-17 22:08:19 +0530

    ui changes

--- src/components'/Card.jsx
+++ src/components'/Card.jsx
@@ -12,16 +12,16 @@ export const Card = ({id,imageId,title,author,category}) => {
     return(
         <div key = {id} className="card">
         <img src={imageId} className="img"/>
-        <h3>Name : {title}</h3>
-        <h3>Author :{author}</h3>
+        <h3 className='center'>Name : {title}</h3>
+        <h3 className='center'>Author :{author}</h3>
         <label htmlFor="category">Move to:</label>
-        <select name="category" id="category" onChange={(e) => changeCategory(e)}>
+        <select name="category" id="category" onChange={(e) => changeCategory(e)} className='optn'>
         <option value="Currently Reading" selected = {category === "Currently Reading"}
-        >Currently Reading
+         className='optn'>Currently Reading
         </option>
-        <option value="Want To Read" selected = {category === "Want To Read"}>Want To Read</option>
-        <option value="Read" selected = {category === "Read"}>Read</option>
-        <option value="None" selected = {category === "None"}>None</option>
+        <option value="Want To Read" selected = {category === "Want To Read"} className='optn'>Want To Read</option>
+        <option value="Read" selected = {category === "Read"} className='optn'>Read</option>
+        <option value="None" selected = {category === "None"} className='optn'>None</option>
         </select>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,12 @@ export const Home = () => {
     const {bookData} = useBooks();
    
     return(
-        <div>
+        <>
             <div className="header">
             <NavLink to ="/"><h3>Home</h3></NavLink>
             <NavLink to ="/search"><h3>Search</h3></NavLink>
             </div>
             <Main/>
-        </div>
+        </>
     )
 }
