f53d782c3f2f819a181089831cebedab12d35367
Vivek Shirsath <vivekshirsath1310@gmail.com>
2023-06-17 19:24:41 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+
+export const books = [
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        price: 10.99,
+        imageId : "https://tse3.mm.bing.net/th?id=OIP.cbgIepMvw8JYPiqXgFCwiwHaLo&pid=Api&P=0&h=180",
+      },
+      {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        price: 12.99,
+        imageId : "https://tse2.mm.bing.net/th?id=OIP.NGofr7d8y6lcAUn2SeXXPgHaLZ&pid=Api&P=0&h=180"
+      },
+      {
+        title: "1984",
+        author: "George Orwell",
+        price: 9.99,
+        imageId : "https://tse3.mm.bing.net/th?id=OIP.OGpI408HGczZB_jSFXZw5gHaLI&pid=Api&P=0&h=180",
+      },
+      {
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        price: 11.99,
+        imageId : "https://tse2.mm.bing.net/th?id=OIP.h0iJZbuWxSjYiIZrHxt1-wHaLZ&pid=Api&P=0&h=180",
+      },
+      {
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        price: 8.99,
+        imageId : "https://tse3.mm.bing.net/th?id=OIP.mhFI3Nd75_9KE06fiw-XGAHaKN&pid=Api&P=0&h=180",
+      },
+      {
+        title: "Do Epic Shit",
+        auhtor : "Ankur",
+        imageId : "https://rukminim1.flixcart.com/image/612/612/kwxv98w0/book/l/z/y/do-epic-shit-original-imag9gcfcwfvwtep.jpeg?q=70",
+      }  
+    
+]
